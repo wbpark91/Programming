1a97a9e92e8c99dfae256df88c37802e90c1790f
wbpark91 <wbpark91@gmail.com>
2017-05-31 20:24:29 +0900

    Second Commitment

--- yieldcurve.py
+++ yieldcurve.py
@@ -14,7 +14,7 @@ from dateutil import relativedelta as reldt
 libor = pd.read_excel('libor.xlsx')
 edf = pd.read_excel('edf.xlsx')
 swap = pd.read_excel('swap.xlsx')
-vol = pd.read_excel('vol.xlsx', index_col = 0) / 100
+vol = pd.read_excel('vol.xlsx', index_col = 0) / 10000
 
 today = dat.datetime(2017, 5, 31)
 #%%
@@ -23,4 +23,13 @@ impvol = np.array(vol['1Yr'][:'2Yr'])
 impvol[5] = impvol[4]
 edf['Vol'] = impvol
 
-edf['T1'] = (edf['FUT_CONTRACT_DT'] - today)
+day = np.zeros(6)
+day_temp = edf['FUT_CONTRACT_DT'] - today
+
+for i in range(len(day)):
+    day[i] = (day_temp[i]).days
+
+edf['T1'] = day / 360
+edf['T2'] = (day + 90) / 360
+
+edf['Fwd'] = (edf['Fut'] / 100) - (0.5 * (edf['Vol'] ** 2) * edf['T1'] * edf['T2'])
