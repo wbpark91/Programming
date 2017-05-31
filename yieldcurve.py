34881262fb1c87de06e0d1101e1067c12e0e53af
wbpark91 <wbpark91@gmail.com>
2017-05-31 21:04:54 +0900

    Add DF

--- yieldcurve.py
+++ yieldcurve.py
@@ -36,7 +36,11 @@ edf['Fwd'] = (edf['Fut'] / 100) - (0.5 * (edf['Vol'] ** 2) \
                                    * edf['T1'] * edf['T2'])
 
 edffwd = edf[['Fwd', 'T1']]['EDU7 Comdty':]
+
+swap['PMT'] = 0.5 * swap['PX_MID']
 #%%
+mat = [0.25, 0.5, 0.75, 1, 1.25, 1.5, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+       11, 12, 15, 20, 25, 30, 40]
 ir = np.zeros(22)
 ir[0] = 4 * np.log(1 + (libor['PX_MID'] / 100) * 0.25)
 
@@ -51,3 +55,10 @@ for i in range(len(edffwd)):
     d = np.exp(-r * t2)
     ir[i+1] = r
     dct[i+1] = d
+
+dct[6] = (100 - swap['PMT'][0] * (dct[1] + dct[3] + dct[5])) / (100 + swap['PMT'][0])
+ir[6] = -np.log(dct[6]) / 2
+    
+#%%
+term = pd.DataFrame(ir, index = mat, columns = ['IR'])
+term['DF'] = np.exp(-term['IR'] * term.index)
